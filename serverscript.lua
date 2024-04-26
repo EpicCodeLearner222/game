b574929a9422f4d0c5913f8886d167c5592deb28
EpicCodeLearner222 <136813735+EpicCodeLearner222@users.noreply.github.com>
2024-04-26 09:22:41 -0400

    Update serverscript.lua

--- serverscript.lua
+++ serverscript.lua
@@ -2,11 +2,26 @@
 
 local players = game.Players
 local lighting = game.Lighting
-
+local dss = game:GetService("DataStoreService")
+local dataStore = dss:GetDataStore("MainGameData",1)
 -- Player Joining Script
 
 players.PlayerAdded:Connect(function(plr)
 local leaderstats = Instance.new("Folder', plr)
       leaderstats.Name = "leaderstats"
-      
+
+                  local cash = Instance.new("NumberValue", leaderstats)
+                  cash.Name = "Cash"
+                  cash.Value = 0
+
+                  local data
+
+                  local s,f = pcall(function()
+                              data = dataStore:GetAsync(plr.UserId.."-cash")
+                        end)
+                  if s then
+                        print("Successfully loaded data!")
+                        cash.Value = data
+
+                  end
   end)
