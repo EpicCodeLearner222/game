e06553c88f547962093cfd74e6787d2e5cc59684
EpicCodeLearner222 <136813735+EpicCodeLearner222@users.noreply.github.com>
2024-04-26 09:26:41 -0400

    Update serverscript.lua

--- serverscript.lua
+++ serverscript.lua
@@ -14,15 +14,23 @@ local leaderstats = Instance.new("Folder', plr)
                   cash.Name = "Cash"
                   cash.Value = 0
 
-                  local data
+                  local settings = Instance.new("Folder", plr)
+                  settings.Name = "Settings"
 
+                  local music = Instance.new("BoolValue", settings)
+                  music.Name = "Music"
+                  music.Value = 0
+
+                  local data
+                  local data2
                   local s,f = pcall(function()
                               data = dataStore:GetAsync(plr.UserId.."-cash")
+                              data2 = dataStore:GetAsync(plr.UserId.."-music
                         end)
                   if s then
                         print("Successfully loaded data!")
                         cash.Value = data
-
+                        music.Value = data2
                   end
   end)
 
@@ -31,6 +39,7 @@ local leaderstats = Instance.new("Folder', plr)
       players.PlayerRemoving:Connect(function(plr)
                   local s,f = pcall(function()
                               dataStore:SetAsync(plr.UserId.."-cash", plr.leaderstats.Cash.Value)
+                                    dataStore:SetAsync(plr.UserId.."-music", plr.Settings.Music.Value)
                         end)
                   if s then
                         print("Saved data")
