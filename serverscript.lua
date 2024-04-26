9e6fbeb03e5fdc5860ee6f3b7e6c31cb26aea9aa
EpicCodeLearner222 <136813735+EpicCodeLearner222@users.noreply.github.com>
2024-04-26 09:21:14 -0400

    Update serverscript.lua

--- serverscript.lua
+++ serverscript.lua
@@ -2,3 +2,11 @@
 
 local players = game.Players
 local lighting = game.Lighting
+
+-- Player Joining Script
+
+players.PlayerAdded:Connect(function(plr)
+local leaderstats = Instance.new("Folder', plr)
+      leaderstats.Name = "leaderstats"
+      
+  end)
